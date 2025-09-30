71d681b49dc5963ab0c8427507a22a0c35dedf82
chyun7114 <chyun7114@naver.com>
2025-10-01 00:10:32 +0900

    Chore: project init

---
+++ src/main/java/com/meetup/loveroute/LoverouteApplication.java
@@ -0,0 +1,13 @@
+package com.meetup.loveroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoverouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoverouteApplication.class, args);
+	}
+
+}
