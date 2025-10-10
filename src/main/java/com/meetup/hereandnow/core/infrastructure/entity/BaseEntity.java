f1a4a566246ad3113abf4f5f2435e65b6b814b08
mminjukim <mminju.km@gmail.com>
2025-10-10 13:22:25 +0900

    #3 fix: BaseEntity 빌더 수정

--- src/main/java/com/meetup/hereandnow/core/infrastructure/entity/BaseEntity.java
+++ src/main/java/com/meetup/hereandnow/core/infrastructure/entity/BaseEntity.java
@@ -3,7 +3,10 @@ package com.meetup.hereandnow.core.infrastructure.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +14,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 @EntityListeners(value = AuditingEntityListener.class)
 public abstract class BaseEntity {
