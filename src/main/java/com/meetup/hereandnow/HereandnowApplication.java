dd4749ccf1965dde7b0b775d5f90b183c3be48ff
mminjukim <mminju.km@gmail.com>
2025-10-10 13:23:38 +0900

    #3 chore: 애플리케이션명 hereandnow로 변경

--- src/main/java/com/meetup/hereandnow/HereandnowApplication.java
+++ src/main/java/com/meetup/hereandnow/HereandnowApplication.java
@@ -2,14 +2,16 @@ package com.meetup.hereandnow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class LoverouteApplication {
+@ConfigurationPropertiesScan
+public class HereandnowApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoverouteApplication.class, args);
+        SpringApplication.run(HereandnowApplication.class, args);
     }
 
 }
