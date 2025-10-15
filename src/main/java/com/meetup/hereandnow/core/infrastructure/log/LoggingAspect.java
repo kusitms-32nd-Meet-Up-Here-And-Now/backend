3015ba9a29526fbab485de3e5ea96f541d01ccbf
chyun7114 <chyun7114@naver.com>
2025-10-15 19:57:09 +0900

    #28 feat: 컨트롤러 level 로깅 추가

---
+++ src/main/java/com/meetup/hereandnow/core/infrastructure/log/LoggingAspect.java
@@ -0,0 +1,51 @@
+package com.meetup.hereandnow.core.infrastructure.log;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+@Aspect
+@Log4j2
+@Component
+@Profile("!local")
+public class LoggingAspect {
+
+    @Pointcut("execution(* com.meetup.hereandnow..presentation.controller..*(..))")
+    public void controller() {}
+
+    @Around("controller()")
+    public Object logController(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        HttpServletRequest request = ((ServletRequestAttributes)(RequestContextHolder.getRequestAttributes())).getRequest();
+        long startTime = System.currentTimeMillis();
+
+        String controllerName = joinPoint.getSignature().getDeclaringType().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        String decodedUri = URLDecoder.decode(request.getRequestURI(), StandardCharsets.UTF_8);
+        try {
+            Object result = joinPoint.proceed();
+            long endTime = System.currentTimeMillis();
+            log.info("[SUCCESS] [{}] {} [Class :{}.{}] time: {}ms",
+                    request.getMethod(), decodedUri, controllerName, methodName, endTime - startTime);
+
+            return result;
+        } catch(Exception e) {
+            long endTime = System.currentTimeMillis();
+            log.info("[FAIL] [{}] {} [Class :{}.{}] time: {}ms",
+                    request.getMethod(), decodedUri, controllerName, methodName, endTime - startTime);
+
+            throw e;
+        }
+
+    }
+}
