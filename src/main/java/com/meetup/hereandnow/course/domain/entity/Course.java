35797d53b4b0e41d637e2de90994841181ac1f83
mminjukim <mminju.km@gmail.com>
2025-10-28 14:17:41 +0900

    #37 merge: develop 최신 코드 병합

--- src/main/java/com/meetup/hereandnow/course/domain/entity/Course.java
+++ src/main/java/com/meetup/hereandnow/course/domain/entity/Course.java
@@ -3,33 +3,16 @@ package com.meetup.hereandnow.course.domain.entity;
 import com.meetup.hereandnow.core.infrastructure.entity.BaseEntity;
 import com.meetup.hereandnow.member.domain.Member;
 import com.meetup.hereandnow.pin.domain.entity.Pin;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
