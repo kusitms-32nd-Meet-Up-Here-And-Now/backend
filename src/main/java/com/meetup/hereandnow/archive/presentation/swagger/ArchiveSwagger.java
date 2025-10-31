cbcf6a5227f2de19b4b61bff819e21c81d109002
mminjukim <mminju.km@gmail.com>
2025-10-31 21:06:46 +0900

    #47 chore: 아카이브 스웨거에 ApiErrorCode 삭제

--- src/main/java/com/meetup/hereandnow/archive/presentation/swagger/ArchiveSwagger.java
+++ src/main/java/com/meetup/hereandnow/archive/presentation/swagger/ArchiveSwagger.java
@@ -2,9 +2,7 @@ package com.meetup.hereandnow.archive.presentation.swagger;
 
 import com.meetup.hereandnow.archive.dto.response.CourseCardDto;
 import com.meetup.hereandnow.archive.dto.response.PlaceCardDto;
-import com.meetup.hereandnow.core.config.swagger.ApiErrorCode;
 import com.meetup.hereandnow.core.presentation.RestResponse;
-import com.meetup.hereandnow.scrap.exception.ScrapErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,7 +17,6 @@ public interface ArchiveSwagger {
             summary = "내가 만든 추억 조회 API",
             operationId = "/archive/created"
     )
-    @ApiErrorCode({ScrapErrorCode.class})
     ResponseEntity<RestResponse<List<CourseCardDto>>> getCreatedCourse(
             @Parameter(description = "페이지 넘버") int page,
             @Parameter(description = "한 페이지당 크기") int size
@@ -29,7 +26,6 @@ public interface ArchiveSwagger {
             summary = "내가 저장한 코스 조회 API",
             operationId = "/archive/scrapped/course"
     )
-    @ApiErrorCode({ScrapErrorCode.class})
     ResponseEntity<RestResponse<List<CourseCardDto>>> getScrappedCourse(
             @Parameter(description = "페이지 넘버") int page,
             @Parameter(description = "한 페이지당 크기") int size
@@ -39,7 +35,6 @@ public interface ArchiveSwagger {
             summary = "내가 저장한 장소 조회 API",
             operationId = "/archive/scrapped/place"
     )
-    @ApiErrorCode({ScrapErrorCode.class})
     ResponseEntity<RestResponse<List<PlaceCardDto>>> getScrappedPlace(
             @Parameter(description = "페이지 넘버") int page,
             @Parameter(description = "한 페이지당 크기") int size
