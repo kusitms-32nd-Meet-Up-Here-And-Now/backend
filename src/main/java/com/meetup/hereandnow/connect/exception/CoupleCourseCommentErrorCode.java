22c15a3588495b1aa41d13ee27239892b05a6f04
chyun7114 <chyun7114@naver.com>
2025-11-06 21:31:35 +0900

    #64 feat: 커플 코스 이미지 댓글 삭제 기능 구현

--- src/main/java/com/meetup/hereandnow/connect/exception/CoupleCourseCommentErrorCode.java
+++ src/main/java/com/meetup/hereandnow/connect/exception/CoupleCourseCommentErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CoupleCourseCommentErrorCode implements BaseErrorCode<DomainException> {
-    NOT_SAVED_IMAGE(HttpStatus.NOT_FOUND, "코멘트 이미지가 저장되지 않았습니다.")
+    NOT_SAVED_IMAGE(HttpStatus.NOT_FOUND, "이미지가 저장되지 않았습니다."),
+    NOT_IMAGE_COMMENT(HttpStatus.BAD_REQUEST, "이미지 댓글이 아닙니다."),
+    FORBIDDEN_COMMENT_DELETE(HttpStatus.UNAUTHORIZED, "본인의 댓글만 삭제할 수 있습니다."),
+    NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다.")
     ;
 
     private final HttpStatus httpStatus;
