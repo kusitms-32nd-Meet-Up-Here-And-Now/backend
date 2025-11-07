0fc78dc126659a0c4d94f81da16e4a577692c90c
mminjukim <mminju.km@gmail.com>
2025-11-07 18:15:15 +0900

    #69 fix: 코스 상세보기 응답에 장소 카테고리 및 url 추가

--- src/main/java/com/meetup/hereandnow/pin/dto/PinDirnameDto.java
+++ src/main/java/com/meetup/hereandnow/pin/dto/PinDirnameDto.java
@@ -7,7 +7,7 @@ public record PinDirnameDto(
         @Schema(name = "핀 순서(1부터 시작)", example = "1")
         int pinIdx,
 
-        @Schema(name = "해당 핀에 대한 사진이 저장되어야하는 디렉토리 이름", example = "/course/{courseId}/pins/{pinId}/images")
+        @Schema(name = "해당 핀에 대한 사진이 저장되어야하는 디렉토리 이름", example = "course/{courseId}/pins/{pinId}/images")
         String pinDirname
 ) {
 }
