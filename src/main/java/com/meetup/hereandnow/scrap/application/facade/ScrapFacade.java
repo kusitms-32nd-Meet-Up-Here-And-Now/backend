2257bbaa5e5d0cfb4919f85e761727e2f0fbd3d8
mminjukim <mminju.km@gmail.com>
2025-11-09 02:09:29 +0900

    #74 feat: 스크랩한 장소 목록 3가지 정렬 포함한 조회 API 구현

--- src/main/java/com/meetup/hereandnow/scrap/application/facade/ScrapFacade.java
+++ src/main/java/com/meetup/hereandnow/scrap/application/facade/ScrapFacade.java
@@ -2,15 +2,21 @@ package com.meetup.hereandnow.scrap.application.facade;
 
 import com.meetup.hereandnow.core.util.SecurityUtils;
 import com.meetup.hereandnow.member.domain.Member;
+import com.meetup.hereandnow.place.application.service.PlaceCardDtoConverter;
+import com.meetup.hereandnow.place.domain.Place;
+import com.meetup.hereandnow.place.dto.PlaceCardResponseDto;
 import com.meetup.hereandnow.scrap.application.service.CourseScrapService;
 import com.meetup.hereandnow.scrap.application.service.PlaceScrapService;
 import com.meetup.hereandnow.scrap.domain.CourseScrap;
 import com.meetup.hereandnow.scrap.domain.PlaceScrap;
 import com.meetup.hereandnow.scrap.dto.response.ScrapResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,6 +25,7 @@ public class ScrapFacade {
 
     private final PlaceScrapService placeScrapService;
     private final CourseScrapService courseScrapService;
+    private final PlaceCardDtoConverter placeCardDtoConverter;
 
     @Transactional
     public ScrapResponseDto toggleScrapCourse(Long courseId) {
@@ -34,11 +41,15 @@ public class ScrapFacade {
     @Transactional
     public ScrapResponseDto toggleScrapPlace(Long placeId) {
         Member member = SecurityUtils.getCurrentMember();
-        Optional<PlaceScrap> placeScrapOptional = placeScrapService.findOptional(member, placeId);
-        if (placeScrapOptional.isPresent()) {
-            return placeScrapService.deleteScrap(placeScrapOptional.get());
-        } else {
-            return placeScrapService.scrap(member, placeId);
-        }
+        return placeScrapService.toggleScrapPlace(member, placeId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PlaceCardResponseDto> getScrappedPlaces(int page, int size, String sort) {
+        Member member = SecurityUtils.getCurrentMember();
+        Pageable resolvedPageable = placeScrapService.resolveSort(page, size, sort);
+        Page<PlaceScrap> scrapPage = placeScrapService.getScrapsByMember(member, resolvedPageable);
+        List<Place> places = scrapPage.getContent().stream().map(PlaceScrap::getPlace).toList();
+        return placeCardDtoConverter.convert(places);
     }
 }
