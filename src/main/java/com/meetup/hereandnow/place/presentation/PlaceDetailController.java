68e68d5eb2bf04c0fc1298400c8fd7664232444c
chyun7114 <chyun7114@naver.com>
2025-11-10 20:09:48 +0900

    #83 feat: 장소 상세 API 구현

---
+++ src/main/java/com/meetup/hereandnow/place/presentation/PlaceDetailController.java
@@ -0,0 +1,31 @@
+package com.meetup.hereandnow.place.presentation;
+
+import com.meetup.hereandnow.core.presentation.RestResponse;
+import com.meetup.hereandnow.place.application.service.PlaceDetailService;
+import com.meetup.hereandnow.place.dto.PlaceInfoResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/place")
+@RequiredArgsConstructor
+public class PlaceDetailController implements PlaceDetailSwagger {
+
+    private final PlaceDetailService placeDetailService;
+
+    @Override
+    @GetMapping("/{placeId}")
+    public ResponseEntity<RestResponse<PlaceInfoResponseDto>> getPlaceDetail(
+            @PathVariable Long placeId
+    ) {
+        return ResponseEntity.ok(
+                new RestResponse<>(
+                        placeDetailService.getPlaceDetail(placeId)
+                )
+        );
+    }
+}
