36b099901bb7f62c67661f478195be765e07842b
chyun7114 <chyun7114@naver.com>
2025-11-10 11:58:31 +0900

    #79 test: 테스트 코드 작 성 및 dto 구현

--- src/main/java/com/meetup/hereandnow/course/application/service/comment/CourseCommentSaveService.java
+++ src/main/java/com/meetup/hereandnow/course/application/service/comment/CourseCommentSaveService.java
@@ -3,6 +3,7 @@ package com.meetup.hereandnow.course.application.service.comment;
 import com.meetup.hereandnow.core.util.SecurityUtils;
 import com.meetup.hereandnow.course.domain.entity.Course;
 import com.meetup.hereandnow.course.domain.entity.CourseComment;
+import com.meetup.hereandnow.course.dto.request.CourseCommentSaveRequestDto;
 import com.meetup.hereandnow.course.exception.CourseErrorCode;
 import com.meetup.hereandnow.course.infrastructure.repository.CourseCommentRepository;
 import com.meetup.hereandnow.course.infrastructure.repository.CourseRepository;
@@ -19,16 +20,16 @@ public class CourseCommentSaveService {
     private final CourseCommentRepository courseCommentRepository;
 
     @Transactional
-    public void saveCourseComment(Long courseId, String content) {
+    public void saveCourseComment(CourseCommentSaveRequestDto courseCommentSaveRequestDto) {
 
         Member member = SecurityUtils.getCurrentMember();
-        Course course = courseRepository.findById(courseId)
+        Course course = courseRepository.findById(courseCommentSaveRequestDto.courseId())
                 .orElseThrow(CourseErrorCode.NOT_FOUND_COURSE::toException);
 
         CourseComment courseComment = CourseComment.builder()
                 .course(course)
                 .member(member)
-                .content(content)
+                .content(courseCommentSaveRequestDto.content())
                 .build();
 
         courseCommentRepository.save(courseComment);
