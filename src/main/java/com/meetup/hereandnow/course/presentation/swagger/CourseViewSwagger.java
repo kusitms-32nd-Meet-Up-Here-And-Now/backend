34e2dc40222bc8d3df28895e5801de431c9de41f
mminjukim <mminju.km@gmail.com>
2025-11-13 14:21:23 +0900

    #53 feat: 홈 화면 api 스웨거 구현

--- src/main/java/com/meetup/hereandnow/course/presentation/swagger/CourseViewSwagger.java
+++ src/main/java/com/meetup/hereandnow/course/presentation/swagger/CourseViewSwagger.java
@@ -2,12 +2,17 @@ package com.meetup.hereandnow.course.presentation.swagger;
 
 import com.meetup.hereandnow.core.config.swagger.ApiErrorCode;
 import com.meetup.hereandnow.core.presentation.RestResponse;
+import com.meetup.hereandnow.course.dto.response.CourseCardResponseDto;
 import com.meetup.hereandnow.course.dto.response.CourseDetailsResponseDto;
 import com.meetup.hereandnow.course.exception.CourseErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Tag(name = "Course", description = "데이트 코스 관련 컨트롤러")
 public interface CourseViewSwagger {
@@ -19,5 +24,20 @@ public interface CourseViewSwagger {
     @ApiErrorCode({CourseErrorCode.class})
     ResponseEntity<RestResponse<CourseDetailsResponseDto>> getCourse(
             @PathVariable Long courseId
+    );
+
+    @Operation(
+            summary = "홈 - 추천 코스 리스트 API",
+            description = "근처 추천 코스 리스트를 반환합니다. sort는 recent, scraps, reviews 입력 가능합니다.",
+            operationId = "/course/home/recommended"
+    )
+    ResponseEntity<RestResponse<List<CourseCardResponseDto>>> getRecommendedCourses(
+            @RequestParam int page,
+            @RequestParam int size,
+            @RequestParam String sort,
+            @Schema(description = "현재 위도", example = "37.5709578373114")
+            @RequestParam double lat,
+            @Schema(description = "현재 경도", example = "126.977928770123")
+            @RequestParam double lon
     );
 }
--- src/main/java/com/meetup/hereandnow/place/presentation/swagger/PlaceHomeSwagger.java
+++ src/main/java/com/meetup/hereandnow/place/presentation/swagger/PlaceHomeSwagger.java
@@ -4,6 +4,7 @@ import com.meetup.hereandnow.core.presentation.RestResponse;
 import com.meetup.hereandnow.place.dto.response.PlaceCardResponseDto;
 import com.meetup.hereandnow.place.dto.response.PlacePointResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,7 +33,9 @@ public interface PlaceHomeSwagger {
             @RequestParam int page,
             @RequestParam int size,
             @RequestParam String sort,
+            @Schema(description = "현재 위도", example = "37.5709578373114")
             @RequestParam double lat,
+            @Schema(description = "현재 경도", example = "126.977928770123")
             @RequestParam double lon
     );
 }
