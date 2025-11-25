30c228e3c7e82b41f884ba7b14e703c98d828601
chyun7114 <chyun7114@naver.com>
2025-11-25 15:02:36 +0900

    #111 Fix: CourseDeleteControllerTest -> IntegrationTest로 변경

--- src/test/java/com/meetup/hereandnow/course/presentation/controller/CourseDeleteControllerTest.java
+++ src/test/java/com/meetup/hereandnow/course/presentation/controller/CourseDeleteControllerTest.java
@@ -2,9 +2,10 @@ package com.meetup.hereandnow.course.presentation.controller;
 
 import com.meetup.hereandnow.core.config.TestSecurityConfiguration;
 import com.meetup.hereandnow.course.application.service.delete.CourseDeleteService;
+import com.meetup.hereandnow.integration.support.IntegrationTestSupport;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,9 +14,8 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = CourseDeleteController.class)
 @Import(TestSecurityConfiguration.class)
-class CourseDeleteControllerTest {
+class CourseDeleteControllerTest extends IntegrationTestSupport {
     @Autowired
     private MockMvc mockMvc;
 
@@ -23,7 +23,8 @@ class CourseDeleteControllerTest {
     private CourseDeleteService courseDeleteService;
 
     @Test
-    void deleteCourse_returnsNoContent_andInvokesService() throws Exception {
+    @DisplayName("DELETE /course/{courseId}")
+    void success_delete_course_by_course_id() throws Exception {
         Long courseId = 1L;
 
         mockMvc.perform(delete("/course/{courseId}", courseId))
